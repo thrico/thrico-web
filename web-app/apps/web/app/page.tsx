80bb408dee7391a52c13dbf68ec1c5eade56a557
Pankaj PulsePlay <pulseplay@Pankajs-MacBook-Pro.local>
2025-04-15 17:39:35 +0530

    test

--- web-app/apps/web/app/page.tsx
+++ web-app/apps/web/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
           <li>
             Get started by editing <code>apps/web/app/page.tsx</code>
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Check if ci/cd working</li>
         </ol>
 
         <div className={styles.ctas}>
